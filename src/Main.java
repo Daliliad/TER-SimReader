45e58399a71f33ff99439ab259b41d4ad02a479c
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-03-19 14:59:08 +0100

    creation d'un bouton play/pause avec timer

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Container;
 import java.io.IOException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.AbstractButton;
+
 
 import display.Board;
 import reader.Reader;
@@ -14,20 +20,60 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		/*Reader r = new Reader("b");
+		
+		Reader r = new Reader("trace.log");
 		int matrice[] = new int[r.getLength()*r.getWidth()];
 		r.readNext(matrice);
-		int colors[] = r.getColors();*/
+		int colors[] = r.getColors();
 		JFrame frame = new JFrame();
 		Board b = new Board();
-		/*b.setWidth(r.getWidth());
+		b.setWidth(r.getWidth());
 		b.setLength(r.getLength());
 		b.setColors(colors);
-		b.setMatrice(matrice);*/
+		b.setMatrice(matrice);
+		
 		JPanel buttons = new JPanel();
-		buttons.setSize(200, 50);
+		
+		JButton play = new JButton("▶");
+		
+		play.addActionListener(new ActionListener() {
+			Timer timer = new Timer();
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int begin = 0;
+				int timeInterval = 1000;
+				Object source = e.getSource();
+					if (source instanceof JButton) {
+						if (play.getText().equals("▶")) {
+							play.setText("❚ ❚ ");
+							timer.schedule(new TimerTask() {
+							
+							@Override
+							public void run() {
+								try {
+									r.readNext(matrice);
+								} catch (IOException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+								b.setMatrice(matrice);
+								System.err.println(matrice.toString());
+								b.revalidate();
+								b.repaint();
+							 }
+						}, begin, timeInterval);
+				    } else if (play.getText().equals("❚ ❚ ")) {
+				          play.setText("▶");
+				          timer.cancel();
+				          timer= new Timer();
+				       }
+					}
+			}
+			
+		});
+		
 		JButton prec = new JButton("prec.");
-		/*prec.addActionListener(new ActionListener() {
+		prec.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -40,9 +86,10 @@ public class Main {
 				b.revalidate();
 				b.repaint();
 			}
-		});*/
+		});
+		
 		JButton suiv = new JButton("suiv.");
-		/*suiv.addActionListener(new ActionListener() {
+		suiv.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -56,17 +103,21 @@ public class Main {
 				b.revalidate();
 				b.repaint();
 			}
-		});*/
+		});
+		
 		buttons.add(prec);
+		buttons.add(play);
 		buttons.add(suiv);
+		
 		frame.add(b);
-		frame.add(buttons,BorderLayout.NORTH);
+		frame.add(buttons,BorderLayout.SOUTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
-		frame.setSize(600, 600);
+		frame.setSize(1100, 600);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo( null );
 		frame.setVisible(true);
+		
 	}
 
 }
