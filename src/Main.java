81557d1c4532a37e1864c1698521948117b3d45d
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-03-30 15:26:58 +0200

    test'

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		  Reader r = new Reader("b");
+		  Reader r = new Reader("trace.log");
 		  int matrice[] = new int[r.getLength()*r.getWidth()];
 		  r.readNext(matrice);
 		  int colors[] = r.getColors();
