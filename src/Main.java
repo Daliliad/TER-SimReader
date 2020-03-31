e318ab3cc2e468391f3e831010a0db8b40552b20
Dalil <dalil.tadjeddine@ens.uvsq.fr>
2020-03-31 05:03:28 +0200

    nouvelle branch

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.AbstractButton;
 import javax.swing.JScrollPane;
@@ -22,7 +25,11 @@ import reader.Reader;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+<<<<<<< Updated upstream
 		Reader r = new Reader("traces/incendie/trace3.log");
+=======
+		Reader r = new Reader("b");
+>>>>>>> Stashed changes
 		//Reader r = new Reader("traces/jeuvie/b");
 		int matrice[] = new int[r.getLength()*r.getWidth()];
 		r.readNext(matrice);
@@ -42,8 +49,14 @@ public class Main {
 
 		JPanel texts = new JPanel();
 		JTextPane logs = new JTextPane();
+<<<<<<< Updated upstream
 		logs.setMaximumSize(new Dimension(1000, 50));
 		logs.setText("test test test test \n"
+=======
+		logs.setMaximumSize(new Dimension(1000, 500));
+		logs.setPreferredSize(new Dimension(1000, 500));
+		/*logs.setText("test test test test \n"
+>>>>>>> Stashed changes
 				+ "test test test test \n"
 				+ "test \n");
 		logs.setEditable(false);
@@ -52,13 +65,13 @@ public class Main {
 		texts.setLayout(new BorderLayout());
 		texts.setMaximumSize(new Dimension(1000, 150));
 		texts.setPreferredSize(new Dimension(300, 150));
-		texts.add(scrlogs);
-		scrlogs.setMaximumSize(new Dimension(1000, 150));
+		texts.add(scrlogs, BorderLayout.NORTH);
+		scrlogs.setMaximumSize(new Dimension(1000, 500));
 
 		JPanel buttons = new JPanel();
 		buttons.setSize(200, 50);
 
-		JButton play = new JButton("▶");
+		JButton play = new JButton("â–¶");
 
 		play.addActionListener(new ActionListener() {
 			Timer timer = new Timer();
@@ -68,8 +81,8 @@ public class Main {
 				int timeInterval = 1000;
 				Object source = e.getSource();
 				if (source instanceof JButton) {
-					if (play.getText().equals("▶")) {
-						play.setText("❚ ❚ ");
+					if (play.getText().equals("â–¶")) {
+						play.setText("â�š â�š ");
 						timer.schedule(new TimerTask() {
 
 							@Override
@@ -86,8 +99,8 @@ public class Main {
 								b.repaint();
 							}
 						}, begin, timeInterval);
-					} else if (play.getText().equals("❚ ❚ ")) {
-						play.setText("▶");
+					} else if (play.getText().equals("â�š â�š ")) {
+						play.setText("â–¶");
 						timer.cancel();
 						timer= new Timer();
 					}
@@ -137,6 +150,36 @@ public class Main {
 		buttons.add(prec);
 		buttons.add(play);
 		buttons.add(suiv);
+		
+		//MenuBar//
+		JMenuBar menu = new JMenuBar();
+		JMenu fileMenu = new JMenu("Fichier");
+		menu.add(fileMenu);
+		JMenuItem openFileMenu = new JMenuItem("Ouvrir Fichier");
+		fileMenu.add(openFileMenu);
+		openFileMenu.addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            //Ajouter Ici l'action de selection d'un fichier
+          }
+      });
+		frame.setJMenuBar(menu);
+		
+		
+		//Second champ pour les infos//
+		JTextPane infos = new JTextPane();
+		infos.setMaximumSize(new Dimension(1000, 500));
+		infos.setPreferredSize(new Dimension(1000, 100));
+		infos.setEditable(false);
+        JScrollPane scrinfo = new JScrollPane(infos);
+        scrinfo.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        texts.add(scrinfo, BorderLayout.SOUTH);
+        scrinfo.setMaximumSize(new Dimension(1000, 500));
+        
+              //-------- A executer a chaque chargement de fichier --------//
+        if(r.logExist(-1))
+          infos.setText(r.getLog(-1));
+		
 
 		frame.add(b);
 		frame.add(buttons,BorderLayout.SOUTH);
