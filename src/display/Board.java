b7c15ef2d1d5bb7336ac75ce68f3d4f88823d600
Flora Carré <flora.carre@gmail.com>
2020-04-01 23:01:49 +0200

    Zoom entièrement modifié. Maintenant fonctionnel et fluide.

--- src/display/Board.java
+++ src/display/Board.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,43 +20,42 @@ import utils.CellType;
 
 public class Board extends JPanel{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private CellType type = CellType.HEXAGONE;
-	private int width = 3;
-	private int length = 3;
-	private int zoom = 1;
-	private int x = 0;
-	private int y = 0;
-	private int[] matrice;
-	private int[] colors;
-	private BufferedImage[] icones;
-	private Dimension caseDimension;
-	
-	public Board() {
-		super();
-		caseDimension = type.defDimension;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private CellType type = CellType.HEXAGONE;
+    private int width = 3;
+    private int length = 3;
+    private int x = 0;
+    private int y = 0;
+    private int[] matrice;
+    private int[] colors;
+    private BufferedImage[] icones;
+    private double zoom;
+
+    public Board() {
+        super();
         this.setPreferredSize();
-	}
-	
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		Dimension d = new Dimension();
-		this.getSize(d);
-		/*if(caseDimension == null) {
-            caseDimension = type.defautDimension(d, width, length);
-        }*/
-		g2d.setStroke((Stroke) new BasicStroke(1, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER));
-		type.paintBoard(g2d, d, caseDimension, matrice, width, length, colors, icones);
-	}
-	
-	/*
+        zoom = 1;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        AffineTransform affineTrans = g2d.getTransform();
+        affineTrans.scale(zoom, zoom);
+        g2d.setTransform(affineTrans);
+        Dimension d = new Dimension();
+        this.getSize(d);
+        g2d.setStroke((Stroke) new BasicStroke(1, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER));
+        type.paintBoard(g2d, d, zoom, matrice, width, length, colors, icones);
+    }
+
+    /*
 	private void drawCells(Graphics2D g2d) {
 		Dimension d = new Dimension();
 		g2d.setColor(Color.black);
@@ -67,64 +67,64 @@ public class Board extends JPanel{
 			g2d.drawLine((d.width-d.height)/2, i*d.height/length, d.width-(d.width-d.height)/2, i*d.height/length);
 		}
 	}
-	*/
-	
-	public void setWidth(int width) {
-		this.width = width;
-        this.setPreferredSize();
-	}
+     */
 
-	public void setLength(int length) {
-		this.length = length;
+    public void setWidth(int width) {
+        this.width = width;
         this.setPreferredSize();
-	}
+    }
 
-	public void setMatrice(int[] matrice) {
-		this.matrice = matrice;
-	}
-
-	public void setColors(int[] colors) {
-		this.colors = colors;
-	}
-	
-	public void setCellType(CellType type) {
-		this.type=type;
+    public void setLength(int length) {
+        this.length = length;
         this.setPreferredSize();
-	}
-	
-	public void setIcones(String dir) {
-		icones = new BufferedImage[colors.length];
-		File f;
-		for(int i=0;i<icones.length;i++) {
-			try {
-				f = new File(Paths.get(Paths.get(dir).toAbsolutePath().toString(),
+    }
+
+    public void setMatrice(int[] matrice) {
+        this.matrice = matrice;
+    }
+
+    public void setColors(int[] colors) {
+        this.colors = colors;
+    }
+
+    public void setCellType(CellType type) {
+        this.type=type;
+        this.setPreferredSize();
+    }
+
+    public void setIcones(String dir) {
+        icones = new BufferedImage[colors.length];
+        File f;
+        for(int i=0;i<icones.length;i++) {
+            try {
+                f = new File(Paths.get(Paths.get(dir).toAbsolutePath().toString(),
                         i+".png").toString());
-				if(f.exists())
-					icones[i] = ImageIO.read(f);
-				else
-					icones[i] = null;
-			} catch (IOException e) {
-				e.printStackTrace();
-				icones[i] = null;
-			}
-		}
-	}
-	
-	public void zoomIn() {
-	    this.type.zoomIn(caseDimension);
-        this.setPreferredSize();
-	    this.revalidate();
-	    this.repaint();
-	}
-	
-	public void zoomOut() {
-        this.type.zoomOut(caseDimension);
+                if(f.exists())
+                    icones[i] = ImageIO.read(f);
+                else
+                    icones[i] = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                icones[i] = null;
+            }
+        }
+    }
+
+    public void zoomIn() {
+        this.zoom = this.type.zoomIn(zoom);
         this.setPreferredSize();
         this.revalidate();
         this.repaint();
     }
-	
-	private void setPreferredSize() {
-        this.setPreferredSize(type.getBoardDimension(width, length, caseDimension));
-	}
+
+    public void zoomOut() {
+        this.zoom = this.type.zoomOut(zoom);
+        this.setPreferredSize();
+        this.revalidate();
+        this.repaint();
+    }
+
+    private void setPreferredSize() {
+        this.setPreferredSize(type.getBoardDimension(width, length, zoom));
+    }
 }
--- src/utils/CellType.java
+++ src/utils/CellType.java
@@ -3,17 +3,18 @@ package utils;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public enum CellType {
-    SQUARE(0, new Dimension(6,6), new Dimension(26,26), new Dimension(150,150)) {
+    SQUARE(0, new Dimension(26,26), 0.4, 5) {
         @Override
         public void paintBoard(Graphics2D g2d,
-                Dimension d, Dimension caseDimension,
+                Dimension d, double zoom,
                 int[] matrice, int width, int length, int[] colors, BufferedImage[] icones) {
             if(matrice != null && matrice.length == width*length && colors != null)
             {
-                int scale = caseDimension.height;
+                int scale = defDimension.height;
                 int xP = 0;
                 int yP = 0;
                 for(int j = 0; j < length; j++) {
@@ -21,7 +22,7 @@ public enum CellType {
                         xP = i*scale;
                         yP = j*scale;
                         g2d.setColor(new Color(colors[matrice[j*width+i]]));
-                        if(d.width/width>15 && icones[matrice[j*width+i]]!=null)
+                        if(zoom>0.5 && icones[matrice[j*width+i]]!=null)
                             g2d.drawImage(icones[matrice[j*width+i]],xP,yP,scale,scale,null);
                         else
                             g2d.fillRect(xP, yP, scale, scale);
@@ -33,55 +34,22 @@ public enum CellType {
         }
 
         @Override
-        public Dimension getBoardDimension(int width, int length, Dimension cellDimension) {
+        public Dimension getBoardDimension(int width, int length, double zoom) {
             Dimension boardDim = new Dimension(
-                    (width+1)*cellDimension.width,
-                    (length+1)*cellDimension.height);
+                    (int) ((width+1)*defDimension.width*zoom) + 1,
+                    (int) ((length+1)*defDimension.height*zoom) + 1);
             return boardDim;
         }
-
-
-        /*
-        @Override
-        public Dimension defautDimension(Dimension panelDimension, int width, int length) {
-            int scale = (panelDimension.width/width<panelDimension.height/length)?
-                    panelDimension.width/width:panelDimension.height/length;
-            Dimension dim = new Dimension(scale, scale);
-            if(dim.height < minDimension.height || dim.width < minDimension.width)
-                return minDimension;
-            if(dim.height > maxDimension.height || dim.width > maxDimension.width)
-                return maxDimension;
-            return dim;
-        }*/
-
-        /*
-        @Override
-        public void zoomIn(Dimension dim) {
-            Dimension newD = new Dimension(dim.width + ZOOM_SIZE, dim.height + ZOOM_SIZE);
-            if(newD.width > maxDimension.width || newD.height > maxDimension.height)
-                dim.setSize(maxDimension);
-            else
-                dim.setSize(newD);
-        }
-
-        @Override
-        public void zoomOut(Dimension dim) {
-            Dimension newD = new Dimension(dim.width - defDimension.width/ZOOM_COEFF_DIV, dim.height - ZOOM_SIZE);
-            if(newD.width < minDimension.width || newD.height < minDimension.height)
-                dim.setSize(minDimension);
-            else
-                dim.setSize(newD);
-        }*/
     },
-    HEXAGONE(1, new Dimension(9,10), new Dimension(24,28), new Dimension(152,175)) {
+    HEXAGONE(1, new Dimension(24,28), 0.4, 5) {
         @Override
         public void paintBoard(Graphics2D g2d,
-                Dimension d, Dimension caseDimension,
+                Dimension d, double zoom,
                 int[] matrice, int width, int length, int[] colors, BufferedImage[] icones) {
             int xP[] = new int[6];
             int yP[] = new int[6];
-            int xScale = caseDimension.width/2;
-            int yScale = caseDimension.height/4;
+            int xScale = defDimension.width/2;
+            int yScale = defDimension.height/4;
             yP[1]=(0*3)*yScale;
             yP[2]=(0*3+1)*yScale;
             //System.out.println(4*yScale);
@@ -98,7 +66,7 @@ public enum CellType {
                     xP[3]=xP[4]=(i*2+1-j%2+2)*xScale;
 
                     g2d.setColor(new Color(colors[matrice[j*width+i]]));
-                    if(4*yScale>15 && icones[matrice[j*width+i]]!=null)
+                    if(zoom>0.5 && icones[matrice[j*width+i]]!=null)
                         g2d.drawImage(icones[matrice[j*width+i]],xP[0],yP[5],2*xScale,4*yScale,null);
                     else
                         g2d.fillPolygon(xP,yP,6);
@@ -109,103 +77,48 @@ public enum CellType {
         }
 
         @Override
-        public Dimension getBoardDimension(int width, int length, Dimension cellDimension) {
+        public Dimension getBoardDimension(int width, int length, double zoom) {
             Dimension boardDim = new Dimension(
-                    (int) ((width+0.5)*cellDimension.width)+1,
-                    (int) (length*(cellDimension.height*3./4)+cellDimension.height*1./4)+1);
+                    (int) ((width+0.5)*defDimension.width*zoom)+1,
+                    (int) (length*(defDimension.height*zoom*3./4)+defDimension.height*zoom*1./4)+1);
             return boardDim;
         }
-
-        /*
-        @Override
-        public Dimension defautDimension(Dimension panelDimension, int width, int length) {
-            int xScale = (int) (((double) panelDimension.width)/(2*width+1));
-            xScale = xScale
-                    + (minDimension.width % ZOOM_SIZE - (xScale*2) % ZOOM_SIZE)/2;
-            int yScale = (int) (xScale/(Math.sqrt(3)));
-            if((yScale*3*length+1)>panelDimension.height) {
-                yScale=(int) (((double) panelDimension.height)/(3*length+1));
-                yScale = yScale
-                        + (minDimension.height % ZOOM_SIZE - (yScale*4) % ZOOM_SIZE)/4;
-                xScale=(int) (yScale*Math.sqrt(3));
-            }
-            
-            xScale = xScale*2;
-            xScale = xScale
-                    + (minDimension.width % ZOOM_SIZE - xScale % ZOOM_SIZE);
-            yScale = yScale*4;
-            yScale = yScale
-                    + (minDimension.height % ZOOM_SIZE - yScale % ZOOM_SIZE);
-            Dimension dim = new Dimension(xScale, yScale);
-            System.out.println("Dimension hexa = "+dim.toString());
-            if(dim.height < minDimension.height || dim.width < minDimension.width)
-                return minDimension;
-            if(dim.height > maxDimension.height || dim.width > maxDimension.width)
-                return maxDimension;
-            return dim;
-        }*/
-
-        /*
-        @Override
-        public void zoomIn(Dimension dim) {
-            Dimension newD = new Dimension(dim.width + defDimension.width/ZOOM_COEFF_DIV, dim.height + defDimension.height/ZOOM_COEFF_DIV);
-            System.out.println(newD);
-            if(newD.width > maxDimension.width || newD.height > maxDimension.height)
-                dim.setSize(maxDimension);
-            else
-                dim.setSize(newD);
-        }
-
-        @Override
-        public void zoomOut(Dimension dim) {
-            Dimension newD = new Dimension(dim.width - defDimension.width/ZOOM_COEFF_DIV, dim.height - defDimension.height/ZOOM_COEFF_DIV);
-            System.out.println(newD);
-            if(newD.width < minDimension.width || newD.height < minDimension.height)
-                dim.setSize(minDimension);
-            else
-                dim.setSize(newD);
-        }*/
     };
 
     public final int i;
-    public final Dimension minDimension;
     public final Dimension defDimension;
-    public final Dimension maxDimension;
+    public final double minZoomOut;
+    public final double maxZoomIn;
     
     public static final double ZOOM_COEFF = 0.15;
 
-    CellType(int i, Dimension min, Dimension def, Dimension max) {
+    CellType(int i, Dimension def, double minZoomOut, double maxZoomIn) {
         this.i = i;
-        this.minDimension = min;
         this.defDimension = def;
-        this.maxDimension = max;
+        this.minZoomOut = minZoomOut;
+        this.maxZoomIn = maxZoomIn;
     }
 
 
     public abstract void paintBoard(Graphics2D g2d,
-            Dimension panelDimension, Dimension caseDimension,
+            Dimension panelDimension, double zoom,
             int[] matrice, int width, int length, int[] colors, BufferedImage[] icones);
 
-    public abstract Dimension getBoardDimension(int width, int length, Dimension cellDimension);
+    public abstract Dimension getBoardDimension(int width, int length, double zoom);
     
-    //public abstract Dimension defautDimension(Dimension panelDimension, int width, int length);
-    
-    public void zoomIn(Dimension dim) {
-        Dimension newD = new Dimension((int) (dim.width*(1+ZOOM_COEFF)), (int) (dim.height*(1+ZOOM_COEFF)));
-        System.out.println(newD);
-        if(newD.width > maxDimension.width || newD.height > maxDimension.height)
-            dim.setSize(maxDimension);
+    public double zoomIn(double zoom) {
+        double newZoom = zoom + 0.05;
+        if(newZoom > maxZoomIn)
+            return maxZoomIn;
         else
-            dim.setSize(newD);
+            return newZoom;
     }
 
-    public void zoomOut(Dimension dim) {
-        Dimension newD = new Dimension((int) (dim.width*(1-ZOOM_COEFF)), (int) (dim.height*(1-ZOOM_COEFF)));
-        System.out.println(newD);
-        if(newD.width < minDimension.width || newD.height < minDimension.height)
-            dim.setSize(minDimension);
+    public double zoomOut(double zoom) {
+        double newZoom = zoom - 0.05;
+        if(newZoom < minZoomOut)
+            return minZoomOut;
         else
-            dim.setSize(newD);
+            return newZoom;
     }
-    
 }
